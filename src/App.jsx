7844713cfe842384b5b41e0a6f35db829a1464e9
Hong <jooimtn@naver.com>
2025-07-30 00:01:42 +0900

    Hash

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import DarkMode from './DarkMode/DarkMode.jsx'
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import { Home } from './Home/Home.jsx';
 function HomePage() {
     return (
@@ -12,14 +12,14 @@ function HomePage() {
 
 function App() {
     return (
-        <BrowserRouter basename="/navicollection.github.io">
+        <HashRouter basename="/navicollection.github.io">
             <div className="darkmode">
                 <DarkMode />
             </div>
             <Routes>
                 <Route path="/" element={<HomePage />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
